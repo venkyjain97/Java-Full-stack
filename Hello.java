3cb01ad55d86fa53fa8559bd2776fbef49237a10
GUNJI VENKATESH <93002292+venkyjain97@users.noreply.github.com>
2021-10-27 11:51:16 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,26 +1,6 @@
-package Practise_Sep_24;
-
 public class Hello {
 
 	public static void main(String args[]) {
-		System.out.println("Hello Worldzzz");
+		System.out.println("Hello World");
 	}
 }
-
-
-// IDE 
-// Integrated Development Environment
-// e.g. Eclipse
-
-
-// JDK -- Java Development Kit
-
-
-// JVM -  Java Virtual Machine
-
-
-// JRE --  Java Runtime Environment
-
-// Write code --> Save your code --> Build Code --> Run our code
-
-// 100 / 0
